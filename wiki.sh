1ce34362db5b68fa30cc9892fc7528dade381e33
Himanshu Shah <himanshu1410@live.com>
2013-07-06 06:05:00 -0400

    Object oriented - Google voice can be swaped with any other text to speech engine- speach function defined in bashrc

--- wiki.sh
+++ wiki.sh
@@ -1,37 +1,10 @@
 #!/bin/bash
-
+. ~/.bashrc
 Query=$(dig +short txt $1.wp.dg.cx \
 | sed -r 's/\http[^ ]*/ /g' )
-
+echo $Query
 if [ "$Query" ]; then
-	python GoogleTTS.py -l en -s $Query
-	echo "$Query\"" | wc 
-	play out.mp3 &
+  speak "$Query"
 else
-
-	python GoogleTTS.py -l en -s "No information Available on $1"
-	play out.mp3 &
-	
-
+	speak "No information Available on $1"
 fi
-
-
-
-#wget -q -U Mozilla -O output.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=$Query\""
-#echo "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=$Query\""
-#play output.mp3
-
-
-
-
-
-
-<<COMMENT1
-
-Query=$(dig +short txt $1.wp.dg.cx \
-| sed -r 's/ +/+/g' \
-| sed -r 's/\.[^ ]*/ /g' \
-| sed -r 's/[,|():]//g' \
-| sed -r 's/ +//g' )
-
-COMMENT1
