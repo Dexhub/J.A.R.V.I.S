81da2b27b3c8bd6bbcda5cf287a6003138003de5
Himanshu Shah <Himanshu1410@live.com>
2015-02-17 16:55:24 -0500

    Code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 J.A.R.V.I.S
 ===========
+>> This willscan the type fo Os for these devices :: sudo nmap -sV -O -v 192.168.1.12
+>> This will scan all the devices :: sudo nmap -sP 192.168.1.0/24
--- search_define.sh
+++ search_define.sh
@@ -6,8 +6,3 @@ wget -qO- -U "Mozilla/6.0 (Macintosh; I; Intel Mac OS X 11_7_9; de-LI; rv:1.9b4)
 sox soundfile.wav -t raw -r 8012 -u -b -c 1 | cat > /dev/dsp
 
 
-
-#wget -qO- -U "Mozilla/6.0 (Macintosh; I; Intel Mac OS X 11_7_9; de-LI; rv:1.9b4) Gecko/2012010317 Firefox/10.0a4" http://www.google.co.uk/search\?q\=$1\&tbs\=dfn:1 \
-#| grep --perl-regexp --only-matching '(?<=<li style="list-style:none">)[^<]+' \
-#| head -1 \
-#| espeak --stdin
